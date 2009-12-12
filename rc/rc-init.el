7ad74d880e4589c276afe0441d1283a1e87b9943
cppiii <cppiii@gmail.com>
2009-12-13 02:37:45 +0800

    change the default font

--- rc/rc-init.el
+++ rc/rc-init.el
@@ -23,7 +23,10 @@
 ;;font
 ;;(set-default-font "-monotype-andale mono-medium-r-normal--0-0-0-0-c-0-adobe-standard")
 ;;(set-default-font "-freetype-coding font tobi-medium-r-normal--16-120-96-96-p-48-iso10646-1")
-(set-default-font "-windows-proggyclean-medium-r-normal--13-80-96-96-c-70-iso8859-1")
+;;(set-default-font "-windows-proggyclean-medium-r-normal--13-80-96-96-c-70-iso8859-1")
+(set-default-font "-windows-dina-medium-r-normal--0-0-96-96-c-0-microsoft-cp1252" )
+
+
 
 ;; move between windows
 ;;(windmove-default-keybindings 'meta)
