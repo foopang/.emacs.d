a3cb70fc6623dc18fba9d9f9bcc76cea3cae34a3
cppiii <cppiii@gmail.com>
2009-12-13 02:57:20 +0800

    change the default font

--- rc/rc-init.el
+++ rc/rc-init.el
@@ -24,7 +24,7 @@
 ;;(set-default-font "-monotype-andale mono-medium-r-normal--0-0-0-0-c-0-adobe-standard")
 ;;(set-default-font "-freetype-coding font tobi-medium-r-normal--16-120-96-96-p-48-iso10646-1")
 ;;(set-default-font "-windows-proggyclean-medium-r-normal--13-80-96-96-c-70-iso8859-1")
-(set-default-font "-windows-dina-medium-r-normal--0-0-96-96-c-0-microsoft-cp1252" )
+(set-default-font "-windows-dina-medium-r-normal--13-80-96-96-c-70-microsoft-cp1252")
 
 
 
