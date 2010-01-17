955be78b0f673d2d09f3398fe80bbe86f8797c53
cppiii <cppiii@gmail.com>
2010-01-17 22:47:45 +0800

    nth

--- rc/rc-init.el
+++ rc/rc-init.el
@@ -177,6 +177,17 @@ buffer read-only, so I suggest setting kill-read-only-ok to t."
 		(let ((mark-even-if-inactive transient-mark-mode))
 		  (indent-region (region-beginning) (region-end) nil))))))
 
+;; transparent window background
+;;(eval-when-compile (require 'cl))
+(defun toggle-transparency ()
+  (interactive)
+  (if (/=
+       (cadr (find 'alpha (frame-parameters nil) :key #'car))
+       100)
+      (set-frame-parameter nil 'alpha '(100 100))
+    (set-frame-parameter nil 'alpha '(85 60))))
+(global-set-key (kbd "C-c C-t") 'toggle-transparency)
+
 ;; anything
 ;;(require 'anything-etags)
 ;;(setq anything-sources
