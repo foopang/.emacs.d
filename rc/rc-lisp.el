7b749597418732356bbd990612488a201f9a5f6b
Eric Cheung <cppiii@gmail.com>
2010-11-19 16:25:25 +0800

    add slime

---
+++ rc/rc-lisp.el
@@ -0,0 +1,36 @@
+;;; rc-lisp.el --- 
+
+;; Copyright (C) 2010  Eric
+
+;; Author: Eric <eric@eric-desktop>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(add-to-list 'load-path "~/.emacs.d/site-lisp/slime")  ; your SLIME directory
+(setq inferior-lisp-program "/usr/bin/sbcl") ; your Lisp system
+(require 'slime)
+(slime-setup)
+
+
+
+
+(provide 'rc-lisp)
+;;; rc-lisp.el ends here
