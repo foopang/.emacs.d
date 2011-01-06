b955cba8fcf4ed89d761bf8da6485e1c5323b28c
Eric Cheung <cppiii@gmail.com>
2011-01-06 18:55:47 +0800

    add flymake checker

--- rc/rc-python.el
+++ rc/rc-python.el
@@ -29,6 +29,26 @@
 (define-key ac-complete-mode-map "\C-n" 'ac-next)
 (define-key ac-complete-mode-map "\C-p" 'ac-previous)
 
+;; Python or python mode?
+(eval-after-load 'python
+  '(progn
+     (when (load "flymake" t) 
+       (defun flymake-pyflakes-init () 
+	 (let* ((temp-file (flymake-init-create-temp-buffer-copy 
+			    'flymake-create-temp-inplace)) 
+		(local-file (file-relative-name 
+			     temp-file 
+			     (file-name-directory buffer-file-name)))) 
+	   (list "pyflakes" (list local-file)))) 
+
+       (add-to-list 'flymake-allowed-file-name-masks 
+		    '("\\.py\\'" flymake-pyflakes-init))) 
+
+     (add-hook 'find-file-hook 'flymake-find-file-hook)
+
+     ))
+
+
 
 ;; Initialize Yasnippet                                                                                        
 ;Don't map TAB to yasnippet                                                                                    
