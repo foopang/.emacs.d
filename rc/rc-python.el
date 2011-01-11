7a4b9c083db45e886ee5c26c1ec93f483e82bac3
Eric Cheung <cppiii@gmail.com>
2011-01-11 15:30:38 +0800

    add pep8 support

--- rc/rc-python.el
+++ rc/rc-python.el
@@ -35,19 +35,35 @@
 ;; Python or python mode?
 (eval-after-load 'python
   '(progn
-     (when (load "flymake" t) 
-       (defun flymake-pyflakes-init () 
-     	 (let* ((temp-file (flymake-init-create-temp-buffer-copy 
-     			    ;;'flymake-create-temp-inplace
-			    'flymake-create-temp-in-system-tempdir
-			    )) 
-     		(local-file (file-relative-name 
-     			     temp-file 
-     			     (file-name-directory buffer-file-name)))) 
-     	   (list "pyflakes" (list local-file)))) 
+     (when (load "flymake" t)
+       (defun flymake-pylint-init (&optional trigger-type)
+	 (let* ((temp-file (flymake-init-create-temp-buffer-copy
+			    'flymake-create-temp-in-system-tempdir))
+		(local-file (file-relative-name
+			     temp-file
+			     (file-name-directory buffer-file-name)))
+		(options (when trigger-type (list "--trigger-type" trigger-type))))
+	   (list "~/.emacs.d/flymake/pyflymake.py" (append options (list local-file)))))
 
-       (add-to-list 'flymake-allowed-file-name-masks 
-     		    '("\\.py\\'" flymake-pyflakes-init)))
+       (add-to-list 'flymake-allowed-file-name-masks
+		    '("\\.py\\'" flymake-pylint-init)))
+
+
+     ;; (when (load "flymake" t) 
+     ;;   (defun flymake-pyflakes-init () 
+     ;; 	 (let* ((temp-file (flymake-init-create-temp-buffer-copy 
+     ;; 			    ;;'flymake-create-temp-inplace
+     ;; 			    'flymake-create-temp-in-system-tempdir
+     ;; 			    )) 
+     ;; 		(local-file (file-relative-name 
+     ;; 			     temp-file 
+     ;; 			     (file-name-directory buffer-file-name)))) 
+     ;; 	   ;;(list "pyflakes" (list local-file))
+     ;; 	   (list "~/.emacs.d/script/pyflymake.py" (list local-file))
+     ;; 	   )) 
+
+     ;;   (add-to-list 'flymake-allowed-file-name-masks 
+     ;; 		    '("\\.py\\'" flymake-pyflakes-init)))
      
      ;;(add-hook 'find-file-hook 'flymake-find-file-hook)
      (add-hook 'python-mode-hook 'flymake-mode)
