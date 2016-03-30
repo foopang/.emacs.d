d355cf534c08af150ee680310b5939e0aec13eda
Foo Pang <foo.pang@101medialab.com>
2016-03-30 09:18:16 +0800

    Custom function eval-and-replace.

--- custom/custom-key-bindings.el
+++ custom/custom-key-bindings.el
@@ -143,4 +143,17 @@
 
 (global-set-key (kbd "C-x C-k") 'kill-buffer-and-window)
 
+;; Eval and Replace (http://emacsredux.com/blog/2013/06/21/eval-and-replace/)
+(defun eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+           (insert (current-kill 0)))))
+
+(global-set-key (kbd "C-c e") 'eval-and-replace)
+
 (provide 'custom-key-bindings)
